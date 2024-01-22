d7fbdbebc89b96256fc6a4ced6fe9fa165d1babf
dahezhiquan <3390205563@qq.com>
2024-01-22 18:35:39 +0800

    feat: 新增poc测试

--- cmd/exp.go
+++ cmd/exp.go
@@ -6,9 +6,13 @@ import (
 	"github.com/spf13/cobra"
 	"io"
 	"log"
+	"sync"
 )
 
 var Lexp = utils.GetSlog("exp")
+var wgs sync.WaitGroup
+var mus sync.Mutex // 互斥锁
+var vulUrls []string
 
 func init() {
 	ew := &utils.EmptyWriter{}
@@ -39,6 +43,34 @@ var expCmd = &cobra.Command{
 		expParameter.Vulname, _ = cmd.Flags().GetString("vulname")
 		if expParameter.Url != "" && expParameter.IsPoc == true {
 			ChoiceMethodPOC(expParameter)
+			return
+		}
+		if expParameter.File != "" && expParameter.IsPoc == true {
+			// 先对文件进行处理
+			utils.ProcessSourceFile(expParameter.File)
+			result, err := utils.ReadLinesFromFile(expParameter.File)
+			if err != nil {
+				Lexp.Fatal("exp列表文件解析失败")
+				return
+			}
+			urlChan := make(chan string)
+			for i := 0; i < expParameter.Thread; i++ {
+				wgs.Add(1)
+				go func() {
+					defer wgs.Done()
+					for url := range urlChan {
+						expParameter.Url = url
+						ChoiceMethodPOC(expParameter)
+					}
+				}()
+			}
+			for _, url := range result {
+				urlChan <- url
+			}
+			close(urlChan)
+			wgs.Wait()
+			utils.WriteFile("poc", vulUrls, false)
+			return
 		}
 	},
 }
@@ -48,6 +80,13 @@ func ChoiceMethodPOC(expParameter utils.ExpParameter) {
 		isVul := poc.V202342793(expParameter)
 		if isVul {
 			Lexp.Info("发现TeamCity 任意代码执行漏洞 " + expParameter.Url)
+			mus.Lock() // 加锁
+			vulUrls = append(vulUrls, expParameter.Url)
+			mus.Unlock() // 解锁
+		} else {
+			Lexp.Fatal("不存在TeamCity 任意代码执行漏洞 " + expParameter.Url)
 		}
+	} else {
+		Lexp.Fatal("未找到此POC")
 	}
 }
